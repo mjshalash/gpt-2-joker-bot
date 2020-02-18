9ca7037a42db812dd60c93a01bfaf119490bd30b
Malik Shalash <mjshalash@rci4u.com>
2020-02-18 14:02:51 -0500

    Added rest of GUI representation for training options
    May need to look at how tkintr classes are set up. Ther emay be a better way to organize them

--- gui/joker.py
+++ gui/joker.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
+from tkinter import ttk, filedialog
 from tkinter import *
 
 
@@ -20,23 +19,38 @@ class Window(Frame):
         Frame.__init__(self, master)
         self.master = master
 
+        # Variables
+        self.pretrainedModel = ''
+        self.device = ''
+        self.datasetFile = ''
+        self.trainedModelFile = ''
+        self.optimizor = ''
+        self.scheduler = ''
+        self.hyperBatch = ''
+        self.hyperEpoch = ''
+        self.hyperLearnRate = ''
+        self.hyperWarmSteps = ''
+        self.hyperTrainSteps = ''
+        self.hyperMaxSeq = ''
+
         # Model Dropdown
+        # TODO: Figure out how to set variable based on this
         self.modelDropLbl = tk.Label(master, text="Pretrained Model:")
         self.modelDrop = ttk.Combobox(master, values=["openai-gpt", "gpt2"])
         self.modelDropLbl.grid(column=0, row=0)
         self.modelDrop.grid(column=0, row=1)
 
         # Device Dropdown
+        # TODO: Figure out how to set variable based on this
         self.deviceDropLbl = tk.Label(master, text="Device:")
         self.deviceDrop = ttk.Combobox(master, values=["cpu", "gpu"])
         self.deviceDropLbl.grid(column=0, row=2)
         self.deviceDrop.grid(column=0, row=3)
 
         # Dataset Browser
-
         self.datasetFinderLbl = tk.Label(master, text="Training Dataset:")
-        self.datasetFinderTxt = Text(
-            master, bg="#FFFFFF", fg="#000000", height=1)
+        self.datasetFinderTxt = Entry(
+            master, textvariable=self.datasetFile)
         self.datasetFinderBtn = Button(
             master, text="Browse", command=self.findDatasetFile)
         self.datasetFinderLbl.grid(column=0, row=4)
@@ -46,8 +60,8 @@ class Window(Frame):
         # Folder to Save Models
         self.saveModelFinderLbl = tk.Label(
             master, text="Trained Models Directory:")
-        self.saveModelFinderTxt = Text(
-            master, bg="#FFFFFF", fg="#000000", height=1)
+        self.saveModelFinderTxt = Entry(
+            master, textvariable=self.trainedModelFile)
         self.saveModelFinderBtn = Button(
             master, text="Browse", command=self.setSaveTrained)
         self.saveModelFinderLbl.grid(column=0, row=7)
@@ -56,14 +70,72 @@ class Window(Frame):
 
         # TODO: Hyperparams, Optimizor, Scheduler
 
-        # Example for textfield
-        # self.resultField = Text(master, bg="#FFFFFF", fg="#000000", height=1)
-        # self.resultField.insert(INSERT, "0")
-        # self.resultField.grid(row=5, columnspan=4)
+        # Optimizor
+        self.optHeader = tk.Label(
+            master, text="Optimizor")
+        self.optTxt = Entry(
+            master, textvariable=self.optimizor)
+
+        # Schedule
+        self.schedHeader = tk.Label(
+            master, text="Scheduler", textvariable=self.scheduler)
+        self.schedTxt = Entry(
+            master)
+
+        # Hyperparameters
+        self.hyperHeader = tk.Label(
+            master, text="Hyperparameters", bg='black', fg='white')
+
+        self.hyperBatchLbl = tk.Label(master, text="Batch Size")
+        self.hyperBatchTxt = Entry(
+            master, textvariable=self.hyperBatch)
+
+        self.hyperEpochLbl = tk.Label(master, text="Epochs")
+        self.hyperEpochTxt = Entry(
+            master, textvariable=self.hyperEpoch)
+
+        self.hyperLearnLbl = tk.Label(master, text="Learning Rate")
+        self.hyperLearnTxt = Entry(
+            master, textvariable=self.hyperLearnRate)
+
+        self.hyperWarmUpLbl = tk.Label(master, text="Warmup Steps")
+        self.hyperWarmUpTxt = Entry(
+            master, textvariable=self.hyperWarmSteps)
+
+        self.hyperTrainingLbl = tk.Label(master, text="Training Steps")
+        self.hyperTrainingTxt = Entry(
+            master, textvariable=self.hyperTrainSteps)
+
+        self.hyperMaxSeqLbl = tk.Label(master, text="Max Sequence")
+        self.hyperMaxSeqTxt = Entry(
+            master, textvariable=self.hyperMaxSeq)
+
+        self.optHeader.grid(column=0, row=10)
+        self.optTxt.grid(column=0, row=11)
+        self.schedHeader.grid(column=0, row=12)
+        self.schedTxt.grid(column=0, row=13)
+        self.hyperHeader.grid(column=0, row=14)
+        self.hyperBatchLbl.grid(column=0, row=15)
+        self.hyperBatchTxt.grid(column=0, row=16)
+        self.hyperEpochLbl.grid(column=0, row=17)
+        self.hyperEpochTxt.grid(column=0, row=18)
+        self.hyperLearnLbl.grid(column=0, row=19)
+        self.hyperLearnTxt.grid(column=0, row=20)
+        self.hyperWarmUpLbl.grid(column=0, row=21)
+        self.hyperWarmUpTxt.grid(column=0, row=22)
+        self.hyperTrainingLbl.grid(column=0, row=23)
+        self.hyperTrainingTxt.grid(column=0, row=24)
+        self.hyperMaxSeqLbl.grid(column=0, row=25)
+        self.hyperMaxSeqTxt.grid(column=0, row=26)
 
 
-root = tk.Tk()
-root.geometry("1000x500")
-app = Window(root)
-root.wm_title("GPT-2 Joker")
-root.mainloop()
+def main():
+    root = tk.Tk()
+    root.geometry("1000x500")
+    Window(root)
+    root.wm_title("GPT-2 Joker")
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
